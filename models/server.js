5cb78e19d2ea525a437ffbab392b6593b190e585
Luis Alberto <alberto.cm4007@gmail.com>
2022-10-03 10:24:08 -0500

    cors

--- models/server.js
+++ models/server.js
@@ -13,6 +13,14 @@ class Server {
 
     middlewares(){
         this.app.use(cors());
+        this.app.use((req, res, next) => {
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+            res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+            res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+            next();
+        });
+        
         //Configuring cookie-parser
         this.app.use(cookieParser()); 
         // Lectura y parseo del body 
