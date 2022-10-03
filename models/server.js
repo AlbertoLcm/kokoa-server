0bc64361a1cd22bb65077036015ceeec296d3e58
Luis Alberto <alberto.cm4007@gmail.com>
2022-10-03 11:00:13 -0500

    origen de cors agregado

--- models/server.js
+++ models/server.js
@@ -12,29 +12,29 @@ class Server {
     }
 
     middlewares(){
-        // this.app.use(
-        //     cors({
-        //         origin: [`http://localhost:${this.port}`, 'https://kokoa-server.herokuapp.com']
-        //     })
-        //   );
-        this.app.use(function (req, res, next) {
+        this.app.use(
+            cors({
+                origin: ['http://127.0.0.1:3000', 'https://kokoafast.herokuapp.com/']
+            })
+          );
+        // this.app.use(function (req, res, next) {
 
-            // Website you wish to allow to connect
-            res.setHeader('Access-Control-Allow-Origin', `http://localhost:3000`);
+        //     // Website you wish to allow to connect
+        //     res.setHeader('Access-Control-Allow-Origin', `http://localhost:3000`);
         
-            // Request methods you wish to allow
-            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        //     // Request methods you wish to allow
+        //     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
         
-            // Request headers you wish to allow
-            res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        //     // Request headers you wish to allow
+        //     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
         
-            // Set to true if you need the website to include cookies in the requests sent
-            // to the API (e.g. in case you use sessions)
-            res.setHeader('Access-Control-Allow-Credentials', true);
+        //     // Set to true if you need the website to include cookies in the requests sent
+        //     // to the API (e.g. in case you use sessions)
+        //     res.setHeader('Access-Control-Allow-Credentials', true);
         
-            // Pass to next layer of middleware
-            next();
-        });
+        //     // Pass to next layer of middleware
+        //     next();
+        // });
         
         //Configuring cookie-parser
         this.app.use(cookieParser()); 
