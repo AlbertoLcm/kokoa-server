3730aa0bb3f519cb977c76bad4cc830fd9bd1ec7
Luis Alberto <alberto.cm4007@gmail.com>
2022-11-09 16:46:34 -0600

    sesion para los cargos mejorado

--- routes/auth.js
+++ routes/auth.js
@@ -46,8 +46,6 @@ routes.post("/login", async (req, res) => {
 
 // ======= Ruta para hacer login a un cargo del usuario =======
 routes.post("/login/cargo", async (req, res) => {
-  const cargo = req.body.cargo;
-  
   try {
     // Paso 1 - Obtenemos toda la informacion del usuario
     const [userGeneral] = await promisePool.query("SELECT * FROM usuarios WHERE id = ?", [req.body.propietario]);
@@ -55,7 +53,9 @@ routes.post("/login/cargo", async (req, res) => {
     const [userCargo] = await promisePool.query(`SELECT * FROM ${req.body.rol} WHERE id = ?`, [req.body.id]);
     // Paso 3 - Creamos el token
     const token = await jwt.sign({
-      id: userGeneral[0].id
+      id: userGeneral[0].id,
+      id_cargo: userCargo[0].id,
+      rol: req.body.rol
     }, process.env.SECRET_KEY, { expiresIn: process.env.JWT_EXPIRE });
 
     return res.cookie("token", token).json({
@@ -63,6 +63,7 @@ routes.post("/login/cargo", async (req, res) => {
       message: "Ingresado correctamente",
       user: {
         token: token,
+        // Aqui manipule la informacion que se envia al front convinandola con el cargo del que inicio sesión
         data: {
           nombre: userGeneral[0].nombre,
           apellidos: userGeneral[0].apellido,
@@ -86,7 +87,41 @@ routes.post("/login/cargo", async (req, res) => {
 });
 // ======= FIN Ruta para hacer login a un cargo del usuario =======
 
-// // ======= Ruta para cerrar session =========
+// Ruta para volver a la cuenta principal desde un cargo
+routes.post("/login/back", async (req, res) => {
+
+  const { id } = req.body;
+  // Paso 1 - Verificamos que ingresen datos
+  if (!id) {
+    return res.status(400).json({ message: "Debes ingresar todos los datos" });
+  }
+  // Paso 2 - Buscamos el usuario
+  const [user] = await promisePool.query("SELECT * FROM usuarios WHERE id = ?", [id]);
+
+  const token = await jwt.sign({
+    id: user[0].id
+  }, process.env.SECRET_KEY, { expiresIn: process.env.JWT_EXPIRE });
+
+  return res.cookie("token", token).json({
+    success: true,
+    message: "Ingresado correctamente",
+    user: {
+      token: token,
+      data: user[0]
+    }
+  });
+  
+  try {
+
+    
+    
+  } catch (error) {
+    res.status(400).json({ message: "Algo salio mal", error: error });
+  }
+});
+
+
+// ======= Ruta para cerrar session =========
 routes.put("/logout", isAuthenticated, (req, res) => {
   const token = req.headers["authorization"];
   jwt.sign(token, "", {
@@ -120,6 +155,32 @@ routes.post("/", isAuthenticated, async (req, res) => {
   const verify = await jwt.verify(token, process.env.SECRET_KEY);
 
   try {
+    if(verify.role !== "usuarios") {
+      const [userCargo] = await promisePool.query(`SELECT * FROM ${verify.rol} WHERE id = ?`, [verify.id]);
+      const [userGeneral] = await promisePool.query("SELECT * FROM usuarios WHERE id = ?", [userCargo[0].propietario]);
+
+      return res.status(200).json({
+        message: "Encontrado",
+        user: {
+          token: token,
+          // Aqui manipule la informacion que se envia al front convinandola con el cargo del que inicio sesión
+          data: {
+            nombre: userGeneral[0].nombre,
+            apellidos: userGeneral[0].apellido,
+            domicilio: userGeneral[0].domicilio,
+            email: userGeneral[0].email,
+            telefono: userGeneral[0].telefono,
+            id_user: userGeneral[0].id,
+            auth: userGeneral[0].auth,
+            id: userCargo[0].id,
+            rol: userCargo[0].rol,
+            nombre_cargo: userCargo[0].nombre,
+            direccion_cargo: userCargo[0].direccion,
+            horario_cargo: userCargo[0].horario,
+          }
+        }
+      });
+    }
     const [user] = await promisePool.query("SELECT * FROM usuarios WHERE id = ?", [verify.id]);
     if (!user.length) {
       return res.status(400).json({ message: "El usuario no exite" });
