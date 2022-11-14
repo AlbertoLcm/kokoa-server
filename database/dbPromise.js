2c0a098a373fb41f10836bd41716ecc9e75e70fd
Luis Alberto <alberto.cm4007@gmail.com>
2022-11-13 18:49:29 -0600

    mejorando

--- database/dbPromise.js
+++ database/dbPromise.js
@@ -1,18 +1,9 @@
 const mysql = require('mysql2');
 
-// create the pool
+// Creamos la conexión pool
 const pool = mysql.createPool(process.env.DATABASE_URL);
-// const pool = mysql.createPool({
-//   host: "au77784bkjx6ipju.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-//   database: "bi2exh0bnxn3lc8h",
-//   user: "v0kcc88z0eppecgm",
-//   password: "rg2tgd6b6nf3oatl",
-//   port: "3306",
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0,
-// });
-// now get a Promise wrapped instance of that pool
+
+// Instanciamos la conexión para obtener una promesa
 const promisePool = pool.promise();
 
 // exportamos wrapped pool
--- routes/upload.js
+++ routes/upload.js
@@ -2,22 +2,25 @@ const express = require('express');
 const routes = express.Router();
 const multer = require('multer');
 const fs = require("fs");
+const promisePool = require('../database/dbPromise');
 
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
     callback(null, './images');
   },
-  filename: (req, file, callback) => {
+  filename: async (req, file, callback) => {
     const ext = file.originalname.split('.').pop();
     const nomImg = `${Date.now()}.${ext}`;
-
+    const nombreCompleto = `https://koko-server.fly.dev/api/upload/${nomImg}`;
+    await promisePool.query('UPDATE usuarios SET ? WHERE id = ?', [{ perfil: nombreCompleto }, req.body.id]);
     callback(null, nomImg);
   }
 });
 
 const upload = multer({ storage });
+
 routes.post('/profile', upload.single('avatar'), (req, res, next) => {
-  res.status(200).json({message: 'Imagen subida correctamente'});
+  res.status(200).json({message: 'Imagen subida correctamente' });
 })
 
 routes.get('/', (req, res) => {
@@ -32,6 +35,19 @@ routes.get('/', (req, res) => {
     files.push(img);
   });
   res.status(200).json(files);
+});
+
+routes.get('/:imagen', (req, res) => {
+
+  // respondemos con la imagen
+  const path = `./images/${req.params.imagen}`;
+
+  const imagen = fs.readFileSync(path);
+  
+  // enviamos la imagen
+  res.writeHead(200, {'Content-Type': 'image/png' });
+  res.end(imagen, 'binary');
+  
 });
 
 module.exports = routes;
